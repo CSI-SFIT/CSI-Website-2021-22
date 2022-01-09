d3d5a3dca0101693c0452f8ddbe61a6ebfc7e141
Varun-Patkar <varunpatkar501@gmail.com>
2022-01-09 12:22:31 +0530

    Fixed wsgi for WhiteNoise

--- CSI-SFIT-Website-21-22/wsgi.py
+++ CSI-SFIT-Website-21-22/wsgi.py
@@ -6,9 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-#wsgi.py
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "CSI-SFIT-Website-21-22.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
