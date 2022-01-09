ec5726cf0649f8e261311bc42f72e9415bec3b72
Varun-Patkar <varunpatkar501@gmail.com>
2022-01-09 12:04:49 +0530

    Preparing for heroku deployment

--- CSI-SFIT-Website-21-22/settings.py
+++ CSI-SFIT-Website-21-22/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-CSI_MONGO_USR = os.environ.get("CSI_MONGO_USR")
-CSI_MONGO_PASS = os.environ.get("CSI_MONGO_PASS")
+SECRET_KEY = os.environ.get("SECRET_KEY") or ""
+CSI_MONGO_USR = os.environ.get("CSI_MONGO_USR") or ""
+CSI_MONGO_PASS = os.environ.get("CSI_MONGO_PASS") or ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
